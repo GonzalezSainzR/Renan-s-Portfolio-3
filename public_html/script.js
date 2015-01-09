7d1d87a04da1fabb54e662397bc737e05f429326
GonzalezSainzR <Raygs2000@gmail.com>
2015-01-09 14:59:20 -0800

    1/9/15

--- public_html/script.js
+++ public_html/script.js
@@ -1,6 +1,36 @@
-$ ("document").ready(function(){
-    $(".yeah").css("background-color", "yellow");
-    
+$("document").ready(function () {
+    $('.yeah').css('background-color', 'skyblue');
+
+    //$('div p:first-child').css('background-color', 'pink');
+
+    //$('p:last').css('color', 'red');
+
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+    $('hi').bind('click', mouseClick);
 });
 
+function mouseOverMe() {
+    $("h1").html("Lol u suck n00b");
+    
+    $(".yeah").css('background-color', 'green');
+    $(".yeah").css('background-color', 'blue');
+    $(".yeah").css('background-color', 'green');
+    $(".yeah").css('background-color', 'blue');
+    $(".yeah").css('background-color', 'green');
+    $(".yeah").css('background-color', 'blue');
+    $(".yeah").css('background-color', 'green');
+    $(".yeah").css('background-color', 'blue');
+    $(".yeah").css('background-color', 'green');
+    $(".yeah").css('background-color', 'blue');
+    
+}
+
+function mouseOutMe() {
+    $('h1').html('Click to find out more');
+}
+
+function mouseClick() {
+    $('p').html('Leedleleedlelee');
+}
 
