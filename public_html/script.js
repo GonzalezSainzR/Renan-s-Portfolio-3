9cba05297a71110d1dc95d6bb397b38932781c87
GonzalezSainzR <Raygs2000@gmail.com>
2015-01-15 14:54:04 -0800

    1/15/15

--- public_html/script.js
+++ public_html/script.js
@@ -1,5 +1,7 @@
 $("document").ready(function () {
     $('.yeah').css('background-color', 'skyblue');
+    
+    $('#superHumans').accordion({header: "h3"});
 
     //$('div p:first-child').css('background-color', 'pink');
 
@@ -7,21 +9,44 @@ $("document").ready(function () {
 
     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
 
-    $('hi').bind('click', mouseClick);
+    $('h1').bind('click', mouseClick);
     
     $('replaceWText').bind('click', replaceWText);
     
     $('#ranPara').bind('click', addAPara);
     
     $('#randPara').bind('click', removeAPara);
+    
+    $('#Logo').bind('click', hideTheImage);
+    
+    $('#show').bind('click', showTheImage);
+    
+    //$('#show').css('visibility', 'hidden');
+
 });
+
+function fadeALittle() {
+    $('#Logo').fadeTo (2500, .30);
+}
+
+function fadeTheImage() {
+    $('#Logo').fadeIn(2500);
+}
+
+function showTheImage () {
+    $('#Logo').show('puff', {}, 2500);
+}
+
+function hideTheImage() {
+    $('#Logo').hide('explode', {}, 2500);
+}
 
 function removeAPara () {
     $('#ranPara p:last').remove();
 }
 
 function addAPara () {
-    $('#ranPara').append('<p>ADDED</p>')
+    $('#ranPara').append('<p>ADDED</p>');
 }
 
 function replaceWText () {
@@ -32,7 +57,6 @@ function mouseOverMe() {
     $("h1").html("Lol u suck n00b");
     
     //$(".yeah").css('background-color', 'green');
-    
 }
 
 function mouseOutMe() {
